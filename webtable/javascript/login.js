665d9742b391c15b9dacb8da82d7f2f86421b95c
SxxHsls80T <trinnakorn2006@gmail.com>
2026-03-27 23:45:08 +0700

    Delete webtable directory

--- webtable/javascript/login.js
+++
@@ -1,8 +0,0 @@
-  function login() {
-            const table = document.getElementById('tableInput').value;
-            if(!table) return alert("Please enter a table number");
-            localStorage.setItem('kitchen_table', table);
-            localStorage.setItem('kitchen_user_id', `T${table}-${Date.now()}`);
-            localStorage.setItem('kitchen_is_paid', 'false');
-            location.href = 'menu.html';
-        }
