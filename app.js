de897f409a41fd933c013bf13d92914624ce0480
Kyo <ngoga2642@gmail.com>
2026-04-03 12:23:57 +0700

    update code main

--- app.js
+++ app.js
@@ -614,4 +614,5 @@ app.post('/cook/register', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Server is running on port 3000');
+    console.log('http://localhost:3000');
 });
